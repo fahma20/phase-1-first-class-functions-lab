aee1b1895fccb8fef323b1c2834c5e93a7c98a5a
fahma20 <fahmaxo@gmail.com>
2024-09-25 13:42:35 +0300

    advanced functions lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+
+const returnFirstTwoDrivers = (drivers) => {
+
+    return ['Antonia', 'Nuru'];
+  };
+
+  const returnLastTwoDrivers = (drivers) => {
+    return ['Amari', 'Mo']
+  };
+
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+  const createFareMultiplier = (multiplier) => {
+    return (fare) => fare * multiplier;
+  };
+
+  const fareDoubler = createFareMultiplier (2);
+
+  const fareTripler = createFareMultiplier (3);
+
+  const selectDifferentDrivers = (drivers, fn) => {
+    return fn(drivers);
+  };
+  
